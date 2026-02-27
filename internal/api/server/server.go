041f560735d97838bb1c669a5fa3aecd1c813f2a
AliasgharHeidari <Aliasgharaliali1213@gmail.com>
2026-02-27 17:21:10 +0330

    feat : added create: session,showtime,movie endpoints

--- internal/api/server/server.go
+++ internal/api/server/server.go
@@ -1,19 +1,21 @@
 package server
 
 import (
+	"cinema-ticket/config"
 	"cinema-ticket/internal/api/handler"
 
 	"github.com/gofiber/fiber/v2"
 )
 
-func Start() {
+func Start(cfg config.ServerConfig) {
 
 	app := fiber.New()
 
 	app.Post("/session", handler.CreateSession) 
+	app.Post("/session/showtime", handler.CreateShowTime)
+	app.Post("/session/movie", handler.CreateMovie)
+	app.Post("/session/room", handler.CreateRoom)
 
-
-
-
+	app.Listen(cfg.Address())
 
 }
