87d0f9cd7f504a5030930c0b303aa196dbbc436b
AliasgharHeidari <Aliasgharaliali1213@gmail.com>
2026-02-27 17:18:25 +0330

    feat: connected to postgres database & automigrate

---
+++ internal/repostiry/postgres.go
@@ -0,0 +1,32 @@
+package postgres
+
+import (
+	"log"
+
+	"cinema-ticket/config"
+	"cinema-ticket/internal/model"
+	"gorm.io/driver/postgres"
+
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDB(cfg config.DatabaseConfig) {
+	dsn := cfg.DSN
+	var err error
+	if DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{}); err != nil {
+		log.Print("failed to connect to database, error:", err)
+	}
+}
+
+func AutoMigrate() {
+	err := DB.AutoMigrate(&model.Movie{}, &model.Room{}, &model.Seat{}, &model.Ticket{}, &model.User{}, &model.Session{}, &model.ShowTime{} )
+	if err != nil {
+		log.Print("failed to automigrate, error:", err)
+	}
+}
+
+func GetDB() *gorm.DB {
+	return DB
+}
